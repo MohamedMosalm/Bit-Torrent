ef2af4d9d6bba0b32b40cad88fd6c8e429af0376
Mohamed Mosalm <mohamed.mosalm2002@gmail.com>
2025-02-01 12:26:20 +0200

    feat: Add torrent file parsing logic

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/MohamedMosalm/Bit-Torrent
 
 go 1.20
+
+require github.com/jackpal/bencode-go v1.0.2 // indirect
---
+++ torrentfile/torrentfile.go
@@ -0,0 +1,84 @@
+package torrentfile
+
+import (
+	"bytes"
+	"crypto/sha1"
+	"fmt"
+	"io"
+
+	"github.com/jackpal/bencode-go"
+)
+
+type bencodeInfo struct {
+	Pieces      string `bencode:"pieces"`
+	PieceLength int    `bencode:"piece length"`
+	Length      int    `bencode:"length"`
+	Name        string `bencode:"name"`
+}
+
+type bencodeTorrent struct {
+	Announce string      `bencode:"announce"`
+	Info     bencodeInfo `bencode:"info"`
+}
+
+type TorrentFile struct {
+	Announce    string
+	InfoHash    [20]byte
+	PieceHashes [][20]byte
+	PieceLength int
+	Length      int
+	Name        string
+}
+
+func Open(r io.Reader) (*bencodeTorrent, error) {
+	bto := bencodeTorrent{}
+	if err := bencode.Unmarshal(r, bto); err != nil {
+		return nil, err
+	}
+	return &bto, nil
+}
+
+func (i *bencodeInfo) hash() ([20]byte, error) {
+	var buf bytes.Buffer
+	if err := bencode.Marshal(&buf, *i); err != nil {
+		return [20]byte{}, nil
+	}
+	return sha1.Sum(buf.Bytes()), nil
+}
+
+func (i *bencodeInfo) splitPieceHashes() ([][20]byte, error) {
+	hashLen := 20
+	buf := []byte(i.Pieces)
+	if len(buf)%hashLen != 0 {
+		err := fmt.Errorf("Received malformed pieces of length %d", len(buf))
+		return nil, err
+	}
+	numHashes := len(buf) / hashLen
+	hashes := make([][20]byte, numHashes)
+
+	for i := 0; i < numHashes; i++ {
+		copy(hashes[i][:], buf[i*hashLen:(i+1)*hashLen])
+	}
+
+	return hashes, nil
+}
+
+func (bto *bencodeTorrent) toTorrentFile() (TorrentFile, error) {
+	infoHash, err := bto.Info.hash()
+	if err != nil {
+		return TorrentFile{}, err
+	}
+	pieceHashes, err := bto.Info.splitPieceHashes()
+	if err != nil {
+		return TorrentFile{}, err
+	}
+	t := TorrentFile{
+		Announce:    bto.Announce,
+		InfoHash:    infoHash,
+		PieceHashes: pieceHashes,
+		PieceLength: bto.Info.PieceLength,
+		Length:      bto.Info.Length,
+		Name:        bto.Info.Name,
+	}
+	return t, nil
+}
